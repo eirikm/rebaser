be9a00984e6a8245f3853f7497bbc8d41d572590
Eirik Meland <eirikm@conduct.no>
2013-01-18 21:23:29 +0100

    working "reword" test

--- src/test/scala/RebaserTest.scala
+++ src/test/scala/RebaserTest.scala
@@ -1,30 +1,34 @@
+import java.io.File
 import java.util
-import org.eclipse.jgit.api.{MergeResult, Git}
+import org.eclipse.jgit.api.RebaseCommand.Action
+import org.eclipse.jgit.api.{RebaseResult, RebaseCommand, Git}
+import org.eclipse.jgit.api.RebaseResult.Status
 import org.eclipse.jgit.diff.DiffEntry
 import org.eclipse.jgit.diff.DiffEntry.ChangeType
-import org.eclipse.jgit.lib.ObjectId
+import org.eclipse.jgit.lib.RepositoryTestCase
 import org.eclipse.jgit.revwalk.RevCommit
-import org.eclipse.jgit.treewalk.{EmptyTreeIterator, TreeWalk}
 import org.junit.Test
 
 
 import org.hamcrest.CoreMatchers.is
-import org.junit.Assert.assertThat
+import org.junit.Assert._
 import org.eclipse.jgit.diff.DiffEntry.DEV_NULL
+import scala.Predef.String
 
 class RebaserTest extends RepositoryTestCase {
 
-  case class File(name: String, content: String)
+  case class GitFile(name: String, content: String)
+
   type CommitMessage = String
 
 
   @Test
-  def initialCommit() {
+  def diffInitialCommit() {
     // arrange
     implicit val git = new Git(db);
     val rebaser: Rebaser = new Rebaser(db)
 
-    val commit: RevCommit = createAddAndCommitFile(File("a.txt", "content"), "initial commit")
+    val commit: RevCommit = createAddAndCommitFile(GitFile("a.txt", "content"), "initial commit")
 
     // act
     val result: util.List[DiffEntry] = rebaser.getAffectedFiles(commit)
@@ -39,13 +43,13 @@ class RebaserTest extends RepositoryTestCase {
 
 
   @Test
-  def normalCommit() {
+  def diffNormalCommit() {
     // arrange
     implicit val git = new Git(db);
     val rebaser: Rebaser = new Rebaser(db)
 
-    createAddAndCommitFile(File("a.txt", "content"), "commit a")
-    val commit = createAddAndCommitFile(File("b.txt", "content"), "commit b")
+    createAddAndCommitFile(GitFile("a.txt", "content"), "commit a")
+    val commit = createAddAndCommitFile(GitFile("b.txt", "content"), "commit b")
 
     // act
     val result: util.List[DiffEntry] = rebaser.getAffectedFiles(commit)
@@ -58,7 +62,40 @@ class RebaserTest extends RepositoryTestCase {
     assertThat(entry.getOldPath(), is(DEV_NULL));
   }
 
-  def createAddAndCommitFile(file: File, commitMsg: CommitMessage)(implicit git: Git): RevCommit = {
+  @Test
+  def testRebaseInteractiveReword {
+    // arrange
+    implicit val git = new Git(db);
+    val rebaser: Rebaser = new Rebaser(db)
+
+    createAddAndCommitFile(GitFile("file1", "content for file1"), "Add file1")
+
+    createAddAndCommitFile(GitFile("file2", "content for file2"), "Add file2")
+
+    createAddAndCommitFile(GitFile("file1", "updated content for file1"), "Updated file1 on master")
+
+    createAddAndCommitFile(GitFile("file2", "updated content for file2"), "updated file2 on side")
+
+    val res: RebaseResult = git.rebase().setUpstream("HEAD~2").runInteractively(new RebaseCommand.InteractiveHandler {
+      def prepareSteps(steps: util.List[RebaseCommand.Step]) {
+        steps.get(0).setAction(Action.REWORD)
+      }
+
+      def modifyCommitMessage(commit: String): String = {
+        return "rewritten commit message"
+      }
+    }).call
+
+    assertTrue(new java.io.File(db.getWorkTree(), "file2").exists());
+    checkFile(new java.io.File(db.getWorkTree(), "file2"), "updated content for file2");
+    assertEquals(Status.OK, res.getStatus());
+    val logIterator: util.Iterator[RevCommit] = git.log().all().call().iterator();
+    logIterator.next(); // skip first commit;
+    val actualCommitMag: String = logIterator.next().getShortMessage();
+    assertEquals("rewritten commit message", actualCommitMag);
+  }
+
+  def createAddAndCommitFile(file: GitFile, commitMsg: CommitMessage)(implicit git: Git): RevCommit = {
     writeTrashFile(file.name, file.content);
     git.add().addFilepattern(file.name).call();
     git.commit().setMessage(commitMsg).call()
