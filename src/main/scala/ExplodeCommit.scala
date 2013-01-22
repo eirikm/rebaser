ed9caaa77f25105803d78d34fb3719ef07868f6c
Eirik Meland <eirikm@conduct.no>
2013-01-22 11:43:03 +0100

    extract methods in ExplodeCommit

--- src/main/scala/ExplodeCommit.scala
+++ src/main/scala/ExplodeCommit.scala
@@ -10,47 +10,58 @@ trait ExplodeCommit extends GitUtilityMethods {
 
   def explodeCommit(commit: RevCommit): Option[RevCommit] = {
     getParentCommit(commit) match {
-      case None => None // TODO treat first commit special
-      case Some(parentCommit) => {
-        val result: RebaseResult = git.rebase().setUpstream(parentCommit).runInteractively(new InteractiveHandler {
-          def prepareSteps(steps: util.List[Step]) {
-            steps.get(0).setAction(Action.EDIT)
-          }
-
-          def modifyCommitMessage(commit: String) = commit
-        }).call()
-
-        println(result.getStatus)
-        println(result.getCurrentCommit.getName)
-        val originalCommitMessage: String = result.getCurrentCommit.getFullMessage
-        println(originalCommitMessage)
-
-        git.reset().setMode(ResetCommand.ResetType.SOFT).setRef("HEAD^").call()
-
-        val status: Status = git.status().call()
-
-        // TODO unstage all modified files
-        // unstage all files
-        val addedFiles: mutable.Set[String] = asScalaSet(status.getAdded())
-        addedFiles foreach unstageFile
-
-        // TODO unstage modified
-        // TODO unstage deleted
-
-        status.getChanged
-
-        // stage one file at a time and commit
-        addedFiles foreach {
-          filename =>
-            git.add().addFilepattern(filename).call()
-            git.commit().setMessage(originalCommitMessage).call()
-        }
-
-        git.rebase().setOperation(RebaseCommand.Operation.CONTINUE).call()
-
-        Some(getHeadCommit)
-      }
+      case None => explodeFirstCommit(commit)
+      case Some(parentCommit) => explodeOtherCommit(parentCommit)
     }
+  }
+
+  private def explodeFirstCommit(firstCommit: RevCommit): Option[RevCommit] =
+    None // TODO treat first commit special
+
+  private def explodeOtherCommit(parentCommit: RevCommit): Option[RevCommit] = {
+    val result: RebaseResult = git.rebase().setUpstream(parentCommit).runInteractively(new InteractiveHandler {
+      def prepareSteps(steps: util.List[Step]) {
+        steps.get(0).setAction(Action.EDIT)
+      }
+
+      def modifyCommitMessage(commit: String) = commit
+    }).call()
+
+    println(result.getStatus)
+    println(result.getCurrentCommit.getName)
+    val originalCommitMessage: String = result.getCurrentCommit.getFullMessage
+    println(originalCommitMessage)
+
+    git.reset().setMode(ResetCommand.ResetType.SOFT).setRef("HEAD^").call()
+
+    val status: Status = git.status().call()
+
+    val addedFiles: mutable.Set[String] = asScalaSet(status.getAdded())
+    val changedFiles: mutable.Set[String] = asScalaSet(status.getChanged())
+    val removedFiles: mutable.Set[String] = asScalaSet(status.getRemoved())
+
+    // unstage all files
+    (addedFiles ++ changedFiles ++ removedFiles).foreach(unstageFile)
+
+    val modifiedFiles: mutable.Set[String] = asScalaSet(status.getModified())
+
+    // stage one file at a time and commit
+    addedFiles foreach {
+      filename =>
+        git.add().addFilepattern(filename).call()
+
+        val lines: Array[String] = originalCommitMessage.split('\n')
+        // TODO use string interpolation
+        lines.update(0, lines.head + " (add " + filename + ")")
+        git.commit().setMessage(lines.mkString("\n")).call()
+    }
+    // TODO commit each changed file
+    // TODO commit each removed file
+    // TODO what about moved files?
+
+    git.rebase().setOperation(RebaseCommand.Operation.CONTINUE).call()
+
+    Some(getHeadCommit)
   }
 
   def unstageFile(filename: String) = {
